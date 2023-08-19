dbac3a45f3bebd80ed73dda89d41b23633b946cb
MinhPhap1508 <minhphapclt.email@gmail.com>
2023-08-19 20:47:11 +0700

    case

---
+++ case_study/task1/src/footer/footer.js
@@ -0,0 +1,32 @@
+import './style.css'
+<nav className="navbar navbar-expand-sm">
+    <div className="container-fluid d-flex justify-content-start">
+        <ul className="navbar-nav">
+            <li className="nav-item">
+                <a className="logo" href="/#">FURAMA</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/#">Home</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/#">Customers</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/#">Rooms</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/#">Spa</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/#">Entertainment</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/#">Service</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/#">Endow</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
