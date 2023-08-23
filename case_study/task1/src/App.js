7b34568170b231f3bc76775557a979c3061688d4
MinhPhap1508 <minhphapclt.email@gmail.com>
2023-08-23 18:56:48 +0700

    case

--- case_study/task1/src/App.js
+++ case_study/task1/src/App.js
@@ -5,6 +5,9 @@ import { CreateService } from './facility/CreateService';
 import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import { EditService } from './facility/EditService';
 import Header from './components/Header';
+import { List } from './customers/List';
+import { CreateCustomer } from './customers/CreateCustomer';
+import { EditCustomer } from './customers/EditCustomer';
 function App() {
   return (
     <Router>
@@ -13,6 +16,9 @@ function App() {
         <Route path='/' element={<Service />} />
         <Route path='/edit-service' element={<EditService/>} />
         <Route path='/create-service' element={<CreateService/>} />
+        <Route path='/list' element={<List/>}/>
+        <Route path='/add-customer' element={<CreateCustomer />}/>
+        <Route path='/edit-customer' element={<EditCustomer />}/>
         </Routes>
     
     </Router>
---
+++ case_study/task1/src/customers/List.js
@@ -0,0 +1,33 @@
+import './list.css';
+export function List(){
+    return(
+        <div class="container">
+        <div class="table-function">
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Birth of day</th>
+                <th>Gender</th>
+                <th>Identity</th>
+                <th>Number Phone</th>
+                <th>Email</th>
+                <th>Kind of customer</th>
+                <th>Address</th>
+                <th>Action</th>
+            </tr>
+            <tr>
+                <td>#</td>
+                <td>#</td>
+                <td>#</td>
+                <td>#</td>
+                <td>#</td>
+                <td>#</td>
+                <td>#</td>
+                <td>#</td>
+                <td>#</td>
+            </tr>
+        </table>
+        </div>
+            </div>
+    )
+}
